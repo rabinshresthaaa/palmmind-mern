1a7ba7797a8fd6e9c008d475f50f1c3ed3bb72d1
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2026-08-12 13:10:35 +0545

    set up app and server

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 
+const routes = require("./routes");
+const errorHandler = require("./middleware/error.middleware");
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Health check
-app.get("/api/health", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Backend API is running",
-  });
-});
+// API routes
+app.use("/api", routes);
+
+app.use(errorHandler);
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,14 @@
 const dotenv = require("dotenv");
 const app = require("./app");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+connectDB();
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port localhost:${PORT}`);
 });
