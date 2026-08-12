b5617a789262741360561cec78d6ef8852fe9f35
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2026-08-12 14:14:21 +0545

    User path setup + password hashing before storing in DB

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -32,6 +33,19 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Hash the password before saving the user
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(12);
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
